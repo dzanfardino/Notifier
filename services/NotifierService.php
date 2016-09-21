f4a3084b20913ed0cd97f6527fa6fc352c3a7ced
Daniele Zanfardino <daniele@electricputty.co.uk>
2016-09-21 16:59:34 +0100

    refactor: notify() in Notifier Service now uses the class of the updated library Classes

--- services/NotifierService.php
+++ services/NotifierService.php
@@ -1,29 +1,47 @@
 <?php
 namespace Craft;
 
-use CodebaseNotifier\Notifier\NotifierFactory;
-use CodebaseNotifier\UseCases\SendErrorString;
+use Airbrake\Notifier;
+use Airbrake\Instance;
+use \Airbrake\ErrorHandler;
 
 class NotifierService extends BaseApplicationComponent
 {
     static protected $settings;
+    protected $notifier;
 
     public function __construct($settings = null)
     {
-        self::$settings = (is_null($settings))
-            ? craft()->plugins->getPlugin('Notifier')->getSettings()
-            : $settings;
+        self::$settings = (is_null($settings)) ? craft()->plugins->getPlugin('Notifier')->getSettings() : $settings;
+
+        // Create new Notifier instance.
+        $this->notifier = new Notifier(array(
+            'host' => self::$settings->airbrakeApiEndpoint,
+            'apiKey' => self::$settings->airbrakeApiKey,
+            'projectId' => self::$settings->airbrakeProjectId,
+            'projectKey' => self::$settings->airbrakeProjectKey,
+        ));
+
+        // Set global notifier instance.
+        Instance::set($this->notifier);
+
+        // Register error and exception handlers.
+        $handler = new ErrorHandler($this->notifier);
+        $handler->register();
+
     }
 
     public function sendString($errorMessage)
     {
-        $notifier = NotifierFactory::getNotifier('airbrake', array(
-            'apiEndPoint' => self::$settings->airbrakeApiEndpoint,
-            'apiKey' => self::$settings->airbrakeApiKey
-        ));
-        $usecase = new SendErrorString($notifier);
-
-        $usecase->execute($errorMessage);
+        // Throw an Exception
+        try
+        {
+            throw new Exception($errorMessage);
+        }
+        catch(Exception $e)
+        {
+            Instance::notify($e);
+        }
     }
 }
 
